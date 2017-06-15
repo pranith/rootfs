f6eb1538e2a5170e209a81f5a781ce361145e9ef
Pranith Kumar <bobby.prani@gmail.com>
2017-06-15 14:47:46 -0400

    Update run script to support x86 and arm64

--- run.sh
+++ run.sh
@@ -1,12 +1,29 @@
 #!/bin/sh
+# Set QEMU_BUILD_DIR to the build directory
 
+QEMU_BUILD_DIR=/home/pranith/qemu/build
+
+ARCH=arm64
 NCPUS=2
 MTTCG=off
+if [ "$1" = "x86" ]; then
+    ARCH=x86
+    QEMU=$QEMU_BUILD_DIR/x86_64-softmmu/qemu-system-x86_64
+    KERNEL_IMG=kernel/linux/arch/x86/boot/bzImage
+    CPU=qemu64
+    MACHINE=pc
+elif [ "$1" = "arm64" ]; then
+    ARCH=arm64
+    QEMU=$QEMU_BUILD_DIR/aarch64-softmmu/qemu-system-aarch64
+    KERNEL_IMG=kernel/linux/arch/arm64/boot/Image
+    CPU=cortex-a57
+    MACHINE=virt,gic_version=3
+fi
 
-cd kernel && ./getkernel.sh && cd ..
-cd initrd && ./getbusybox.sh && cd ..
+cd kernel && ./getkernel.sh $ARCH && cd ..
+cd initrd && ./getbusybox.sh $ARCH && cd ..
 
 echo === RUNNING QEMU ===
-$QEMU -kernel kernel/linux/arch/arm64/boot/Image -initrd initrd/initrd.cpio \
-      -m 512 -M virt,gic_version=3 -cpu cortex-a57 -append "init=/init console=ttyAMA0 console=ttyS0 ignore_loglevel initcall_debug=1" \
+$QEMU -kernel $KERNEL_IMG -initrd initrd/initrd.cpio \
+      -m 512 -M $MACHINE -cpu $CPU -append "init=/init console=ttyAMA0 console=ttyS0 ignore_loglevel initcall_debug=1" \
       -nographic -smp $NCPUS
